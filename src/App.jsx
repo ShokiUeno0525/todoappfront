b5c86c7b04fa57edd84c3cd8badffff85143f9ca
Shoki Ueno <shokiueno.ac@gmail.com>
2025-06-09 22:30:52 +0900

    Reactルーティング

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
 import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { fetchCurrentUser } from './api/auth';
+import LoginPage from './pages/loginpage';
+import RegisterPage from  './apges/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] =useState(true);
+
+  //起動時にログイン済か確認
+  useEffect(() => {
+    (async () =>{
+      try{
+        const data = await fetchCurrentUser();
+        setUser(data);
+     } catch (e) {
+      setUser(null);
+     } finally {
+      setLoading(false);
+     }
+    })();
+  }, []);
+
+  if (loading) {
+    return<div>Loading...</div>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+     <Routes>
+      <Route
+       path="/login"
+       element={!user ? <LoginPage setUser={setUser} /> : <Navigate to="/dashboard" replace />}
+       />
+      <Route
+        path="/register"
+        element={!user ? <RegisterPage setUser={setUser} /> : <Navigate to="/dashboard" replace />}
+        />
+      <Route
+       path="/register"
+       element={user ? <DashboardPage user={user} /> : <Navigate to="/login" replace />}
+       />
+       //その他のルート追加してもいいよ
+    　</Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
