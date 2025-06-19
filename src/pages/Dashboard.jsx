f7ebc80848ff43f6230b5cd75b2cdba1f93a41da
Shoki Ueno <shokiueno.ac@gmail.com>
2025-06-19 23:15:20 +0900

    ”認証周り修正”

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import TodoList from '../components/TodoList';
+import TodoForm from '../components/TodoForm';
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>My ToDo Dashboard</h1>
+      <TodoForm />   {/* 追加用フォームコンポーネント */}
+      <TodoList />   {/* 上で作った一覧コンポ넌트 */}
+    </div>
+  );
+}
